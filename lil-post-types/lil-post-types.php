f9238e0b526fd451324b1fa628c3aa0d2d9e7dee
Rohit Vasudevamurthy <rohitvasudevamurthy@gmail.com>
2022-12-11 19:33:13 -0500

    Inital Commit

---
+++ lil-post-types/lil-post-types.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:       Business Directory Post Types and Taxonomies
+ * Plugin URI:        http://github.com/jcasabona/lil-post-types/
+ * Description:       A simple plugin for creating custom post types and taxonomies related to a business directory.
+ * Version:           1.0.0
+ * Author:            Joe Casabona
+ * Author URI:        https:/casabona.org/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       lil-post-types
+ * Domain Path:       /languages
+ */
